ee85d0766ade318c0f42cd3e46ef9121388d8e04
dhonstudio <doon13@gmail.com>
2023-01-27 10:38:58 +0700

    test add

--- src/DhonBase.php
+++ src/DhonBase.php
@@ -5,9 +5,11 @@ namespace Dhonstudio\Ci4basec;
 class DhonBase
 {
     public $assets;
+    public $api_url;
 
     public function __construct()
     {
         $this->assets = getenv('app.assetsURL');
+        $this->api_url = getenv('app.apiURL');
     }
 }
