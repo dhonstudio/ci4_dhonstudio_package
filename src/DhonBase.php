dee67aac987941d395fbfa2a2b59e0c86c0a275f
dhonstudio <doon13@gmail.com>
2023-01-27 10:50:43 +0700

    add variable

--- src/DhonBase.php
+++ src/DhonBase.php
@@ -4,12 +4,65 @@ namespace Dhonstudio\Ci4basec;
 
 class DhonBase
 {
+    /**
+     * Assets URL.
+     *
+     * @var string
+     */
     public $assets;
+
+    /**
+     * API URL.
+     *
+     * @var string
+     */
     public $api_url;
+
+    /**
+     * @var string
+     */
+    public $userSessionName;
+
+    /**
+     * User session expiration.
+     * 
+     * @var int
+     */
+    public $userSessionExpiration;
+
+    /**
+     * @var string
+     */
+    public $redirectSessionName;
+
+    /**
+     * Redirect session expiration.
+     * 
+     * @var int
+     */
+    public $redirectSessionExpiration;
+
+    /**
+     * @var string
+     */
+    public $bearerTokenSessionName;
+
+    /**
+     * Bearer session expiration.
+     * 
+     * @var int
+     */
+    public $bearerTokenSessionExpiration;
 
     public function __construct()
     {
         $this->assets = getenv('app.assetsURL');
         $this->api_url = getenv('app.apiURL');
+        $this->userSessionName = getenv('session.userName');
+        $this->userSessionExpiration = getenv('session.userExpiration');
+        $this->redirectSessionName = getenv('session.redirectName');
+        $this->redirectSessionExpiration = getenv('session.redirectExpiration');
+        $this->bearerTokenSessionName = getenv('session.bearerName');
+        $this->bearerTokenSessionExpiration = getenv('session.bearerExpiration');
     }
 }
